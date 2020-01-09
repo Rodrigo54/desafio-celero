a721efe582f3cd7d68774d8d408ea2f11ba9b61f
Rodrigo Alves Mesquita <rodrigo54mix@gmail.com>
2020-01-09 15:48:58 -0300

    improve player service

--- src/app/core/player.service.ts
+++ src/app/core/player.service.ts
@@ -3,6 +3,14 @@ import { Injectable } from '@angular/core';
 import { environment } from '@environments/environment';
 import MD5 from 'crypto-js/md5';
 import { map } from 'rxjs/operators';
+import { BehaviorSubject } from 'rxjs';
+
+interface Player {
+  thumbnail: string;
+  name: string;
+  type: 'X' | 'O' | null;
+  wins: number;
+}
 
 @Injectable({
   providedIn: 'root'
@@ -12,14 +20,56 @@ export class PlayerService {
   privateKey: string;
   publicKey: string;
 
+  private player1: Player;
+  private player2: Player;
+
+  private playerStartSubject = new BehaviorSubject(undefined);
+
   constructor(public http: HttpClient) {
     this.endPoint = environment.API_END_POINT;
     this.publicKey = environment.MARVEL_PUBLIC_KEY;
     this.privateKey = environment.MARVEL_PRIVATE_KEY;
   }
 
-  get playerSelected$() {
-    return false;
+  get playerStart$() {
+    return this.playerStartSubject.asObservable();
+  }
+
+  getPlayer(player: 1 | 2) {
+    return player === 1 ? this.player1 : this.player2;
+  }
+
+  setPlayer(value: Omit<Player, 'type' | 'wins'>, player: 1 | 2) {
+    if (player === 1) {
+      this.player1 = {
+        ...value,
+        wins: 0,
+        type: null
+      };
+    }
+    if (player === 2) {
+      this.player2 = {
+        ...value,
+        wins: 0,
+        type: null
+      };
+    }
+  }
+
+  start() {
+    this.defineTypes();
+    this.playerStartSubject.next(true);
+  }
+
+  defineTypes() {
+    const num = Math.floor(Math.random() * (1000 - 1)) + 1;
+    if (num % 2 === 0) {
+      this.player1.type = 'X';
+      this.player2.type = 'O';
+    } else {
+      this.player1.type = 'O';
+      this.player2.type = 'X';
+    }
   }
 
   search(text: string) {
