94f39cd1202ab66b459072f9b02421e8af9d4f89
Rodrigo Alves Mesquita <rodrigo54mix@gmail.com>
2020-01-09 18:07:14 -0300

    improve player service

--- src/app/core/player.service.ts
+++ src/app/core/player.service.ts
@@ -2,10 +2,10 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '@environments/environment';
 import MD5 from 'crypto-js/md5';
-import { map } from 'rxjs/operators';
 import { BehaviorSubject } from 'rxjs';
+import { map } from 'rxjs/operators';
 
-interface Player {
+export interface Player {
   thumbnail: string;
   name: string;
   type: 'X' | 'O' | null;
@@ -20,9 +20,22 @@ export class PlayerService {
   privateKey: string;
   publicKey: string;
 
-  private player1: Player;
-  private player2: Player;
-
+  private playersSubject = new BehaviorSubject<{ 1: Player, 2: Player }>(
+    {
+      1: {
+        thumbnail: 'http://i.annihil.us/u/prod/marvel/i/mg/9/c0/527bb7b37ff55.jpg',
+        name: 'Iron Man',
+        wins: 0,
+        type: 'X'
+      },
+      2: {
+        thumbnail: 'http://i.annihil.us/u/prod/marvel/i/mg/3/50/526548a343e4b.jpg',
+        name: 'Spider-Man',
+        wins: 0,
+        type: 'O'
+      }
+    }
+  );
   private playerStartSubject = new BehaviorSubject(undefined);
 
   constructor(public http: HttpClient) {
@@ -35,25 +48,45 @@ export class PlayerService {
     return this.playerStartSubject.asObservable();
   }
 
-  getPlayer(player: 1 | 2) {
-    return player === 1 ? this.player1 : this.player2;
+  getPlayer(player: 'X' | 'O') {
+    return this.playersSubject.asObservable().pipe(
+      map(players => Object.values(players).find(p => p.type === player))
+    );
+  }
+
+  get players$() {
+    return this.playersSubject.asObservable();
   }
 
   setPlayer(value: Omit<Player, 'type' | 'wins'>, player: 1 | 2) {
+    let player1: Player;
+    let player2: Player;
     if (player === 1) {
-      this.player1 = {
+      player1 = {
         ...value,
         wins: 0,
         type: null
       };
     }
     if (player === 2) {
-      this.player2 = {
+      player2 = {
         ...value,
         wins: 0,
         type: null
       };
     }
+    this.playersSubject.next({ 1: player1, 2: player2 });
+  }
+
+  addPoint(player: 'X' | 'O') {
+    const { 1: player1, 2: player2 } = this.playersSubject.getValue();
+    if (player1.type === player) {
+      player1.wins = player1.wins + 1;
+    }
+    if (player2.type === player) {
+      player2.wins = player2.wins + 1;
+    }
+    this.playersSubject.next({ 1: player1, 2: player2 });
   }
 
   start() {
@@ -62,14 +95,16 @@ export class PlayerService {
   }
 
   defineTypes() {
+    const { 1: player1, 2: player2 } = this.playersSubject.getValue();
     const num = Math.floor(Math.random() * (1000 - 1)) + 1;
     if (num % 2 === 0) {
-      this.player1.type = 'X';
-      this.player2.type = 'O';
+      player1.type = 'X';
+      player2.type = 'O';
     } else {
-      this.player1.type = 'O';
-      this.player2.type = 'X';
+      player1.type = 'O';
+      player2.type = 'X';
     }
+    this.playersSubject.next({ 1: player1, 2: player2 });
   }
 
   search(text: string) {
