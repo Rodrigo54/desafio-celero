2a28ead0ee0c9b2230d6ebeb06759b8b7b638462
Rodrigo Alves Mesquita <rodrigo54mix@gmail.com>
2020-01-10 17:05:24 -0300

    add tests game service

--- src/app/core/game.service.spec.ts
+++ src/app/core/game.service.spec.ts
@@ -16,4 +16,45 @@ describe('GameService', () => {
     const service: GameService = TestBed.get(GameService);
     expect(service).toBeTruthy();
   });
+
+  it('check [ X ] is wins', () => {
+    const service: GameService = TestBed.get(GameService);
+    service.insert(0, 0); // x
+    service.insert(0, 1); // o
+    service.insert(1, 1); // x
+    service.insert(1, 0); // o
+    service.insert(2, 2); // x
+    service.endGame$.subscribe(endGame => {
+      expect(endGame).toEqual({ player: 'X', victory: true });
+    });
+  });
+
+  it('check [ O ] is wins', () => {
+    const service: GameService = TestBed.get(GameService);
+    service.insert(0, 0); // x
+    service.insert(1, 1); // o
+    service.insert(0, 1); // x
+    service.insert(0, 2); // o
+    service.insert(1, 2); // x
+    service.insert(2, 0); // o
+    service.endGame$.subscribe(endGame => {
+      expect(endGame).toEqual({ player: 'O', victory: true });
+    });
+  });
+
+  it('check empate', () => {
+    const service: GameService = TestBed.get(GameService);
+    service.insert(0, 0); // x
+    service.insert(1, 1); // o
+    service.insert(0, 1); // x
+    service.insert(0, 2); // o
+    service.insert(2, 0); // x
+    service.insert(1, 0); // o
+    service.insert(1, 2); // x
+    service.insert(2, 2); // o
+    service.insert(2, 1); // x
+    service.endGame$.subscribe(endGame => {
+      expect(endGame).toEqual({ draw: true });
+    });
+  });
 });
